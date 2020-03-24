c2f864586f7032e798827020887767cb617a153f
Lucas Soares Granja <lucassoaresgranja@yahoo.com.br>
2020-03-24 16:48:48 -0300

    chore: altera retorno da rota

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/teste',(req,res) => {
-    return res.json({hello: 'world'})
+    return res.json({hello: 'world 2'})
 });
 app.listen(3333);
